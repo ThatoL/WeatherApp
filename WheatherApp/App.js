b778532e1799a213cc13e682e4e3a4876f17566c
ThatoL <t.lebethe93@gmail.com>
2022-06-19 18:28:24 +0200

    search component rendered in main UI

--- WheatherApp/App.js
+++ WheatherApp/App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/components/Header'
+import { StyleSheet, View , TextInput, TouchableOpacity, ImageBackground} from 'react-native';
+import Header from './src/components/Header';
+import Search from './src/components/Search';
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
-
+      <Search />
       
       <StatusBar style="auto" />
     </View>
@@ -14,10 +17,10 @@ export default function App() {
 }
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
+    flex: 1,
     // backgroundColor: '#fff',
     // alignItems: 'center',
     // justifyContent: 'center',
   },
-
+ 
 });
