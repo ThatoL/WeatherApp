dd4a99a625ab7b21ecad1d68c7fcb021083caf4a
ThatoL <t.lebethe93@gmail.com>
2022-06-24 17:36:28 +0200

    cleaned up code into own component

--- WheatherApp/App.js
+++ WheatherApp/App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View , Text,  ImageBackground} from 'react-native';
 import Header from './src/components/Header';
 import Search from './src/components/Search';
+import CardContainer from './src/components/CardContainer';
 
 
 
@@ -10,20 +11,9 @@ export default function App() {
     <View style={styles.container}>
       <Header />
       <Search />
-      <View style={styles.cardContainer}>
-      <Text style={styles.headerText}>Added Location</Text>
-        <ImageBackground source={require('./src/images/spring.jpg')}>
-          <View style={styles.weatherCard}>
-            <View style={styles.location}>
-              <Text style={styles.city}>JohannesBurg</Text>
-              <Text style={styles.country}>Republic of South Africa</Text>
-            </View>
-            <View style={styles.tempContainer}>
-              <Text style={styles.tempText}>5 °C</Text>
-            </View>
-          </View>
-        </ImageBackground>
-      </View>
+      <CardContainer />
+      
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -35,51 +25,6 @@ const styles = StyleSheet.create({
     // alignItems: 'center',
     // justifyContent: 'center',
   },
-  cardContainer : {
-    margin: 20,
-  },
-  headerText : {
-    fontSize: 15,
-    color: 'grey',
-    fontWeight: '500',
-    marginBottom: 10,
-  },
-  weatherCard : {
-    flexDirection: 'row',
-    marginTop: 10,
-    height: 90,
-    //backgroundColor: 'pink',
-    shadowColor: 'black',
-    shadowOffset: {width: 5, height: 5},
-    elevation: 7,
-    shadowOpacity: 0.7,
-    borderRadius: 20,
-  },
-  location: {
-    marginTop: 20,
-    padding: 10,
-  },
-  city: {
-    fontSize: 18,
-    fontWeight: '500',
-    color: 'white'
-
-  }, 
-  country: {
-    fontSize: 15,
-    fontWeight: '400',
-    color: 'white'
-  },
-  tempContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-  },
-  tempText: {
-    fontSize: 25,
-    marginTop: 25,
-    color: 'white'
-  }
-
+  
  
 });
